b3dda49208e51a61a5b729cab94bfc2297186e9b
Stirod08 <s.rodriguez4926@gmail.com>
2025-06-30 11:08:34 -0500

    Inicializando proyecto y configuraciones basicas.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <header className='bg-blue-500 py-8 max-h-72'>
+        <h1 className='text-4xl text-white text-center font-black uppercase'>Planificador de gastos</h1>
+      </header>
+
+      <section className='max-w-3xl mx-auto bg-black shadow-lg rounded-lg mt-10 p-10'></section>
+    </>
+  );
+}
